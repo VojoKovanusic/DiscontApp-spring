b0b4452ec1e39da21393be9689b01da0ce735f27
Vojo <mladisumar@gmail.com>
2019-01-04 18:25:40 +0100

    rename

--- src/main/java/com/discont/bean/AppBeans.java
+++ src/main/java/com/discont/bean/AppBeans.java
@@ -1,11 +1,11 @@
-package com.discont.been;
+package com.discont.bean;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class AppBeens {
+public class AppBeans {
 
 	@Bean
 	RestTemplate restTemplateFactory() {
